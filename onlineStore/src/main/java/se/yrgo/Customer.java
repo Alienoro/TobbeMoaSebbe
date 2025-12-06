e70f69a519b923993898b65edd0991383fd5b797
Sebbe <sebban952@gmail.com>
2025-12-06 17:52:15 +0100

    adding Customer.java to main branch

---
+++ onlineStore/src/main/java/se/yrgo/Customer.java
@@ -0,0 +1,57 @@
+package se.yrgo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Customer {
+
+    private int customerID;
+    private String name;
+    private String email;
+    private List<List<Product>> orderHistory = new ArrayList<>();
+
+    public Customer(int customerID, String name, String email) {
+        this.customerID = customerID;
+        this.name = name;
+        this.email = email;
+    }
+
+    public void placeOrder(List<Product> order) {
+        orderHistory.add(order);
+    }
+
+    public void viewOrderHistory() {
+        for (List<Product> order : orderHistory) {
+            for (Product p : order) {
+                System.out.println(p);
+            }
+        }
+    }
+
+    public static String readInput(Scanner sc, String type) {
+        String input;
+        while (true) {
+            System.out.print(type + ": ");
+            input = sc.nextLine();
+
+            if (type.equalsIgnoreCase("Namn") && input.matches("[a-zA-ZÅÄÖåäö\\- ]+")) {
+                return input;
+            } else if (type.equalsIgnoreCase("Email") && input.contains("@")) {
+                return input;
+            } else {
+                if (type.equalsIgnoreCase("Namn")) {
+                    System.out.println("Ett namn får bara innehålla bokstäver");
+                } else if (type.equalsIgnoreCase("Email")) {
+                    System.out.println("Måste vara en giltig email adress.");
+                }
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Customer [customerID=" + customerID + ", name=" + name + ", email=" + email + "]";
+    }
+}
+
