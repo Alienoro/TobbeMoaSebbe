50dcd6018f19413694f4cdf1d431a3ae6ad9b8f9
Your Name <you@example.com>
2025-12-08 20:36:05 +0100

    adding javadocs to shoppingCart class

--- onlineStore/src/main/java/se/yrgo/ShoppingCart.java
+++ onlineStore/src/main/java/se/yrgo/ShoppingCart.java
@@ -2,18 +2,37 @@ package se.yrgo;
 
 import java.util.ArrayList;
 import java.util.List;
-
+/**
+ * The class represents a shopping cart that contains a list of products.
+ * It provides methods to add products, retrieve the cart's content, and calculate the total price of all products.
+ */
 public class ShoppingCart {
     private List<Product> shoppingCart;
 
+    /**
+     * Constructs an empty shopping cart.
+     * Initializes the shopping cart as an empty list.
+     */
     public ShoppingCart() {
         this.shoppingCart = new ArrayList<>();
     }
 
+    /**
+     * Retrieves the list of products in the shopping cart.
+     *
+     * @return a new list containing the products currently in the cart.
+     */
     public List<Product> getShoppingCart() {
         return new ArrayList<>(shoppingCart);
     }
 
+    /**
+     * Adds a product to the shopping cart.
+     * If the provided product is null, an error message will be printed.
+     *
+     * @param product the product to be added to the cart.
+     * @return true if the product was successfully added, false if the product is null.
+     */
     public boolean addProduct(Product product) {
         if (product == null) {
             System.out.println("Kunde inte hitta produkten");
@@ -24,6 +43,11 @@ public class ShoppingCart {
     }
 
 
+    /**
+     * Calculates the total price of all products in the shopping cart.
+     *
+     * @return the total price of all products in the cart.
+     */
     public double calculateTotalPrice() {
         double sum = 0;
 
